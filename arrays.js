2cd53a20d93bec7020b76d8a685d4519964449a1
Cristian <cristiannicolas32@hotmail.com>
2020-11-09 18:10:50 -0300

    Modifique los archivos

---
+++ arrays.js
@@ -0,0 +1,4 @@
+let numero1 = 9 
+let numero2 = 6
+let frutas = ['Naranja', 'Frutilla', 'Kiwi', 'Anana']
+let verduras = ['Acelga', 'Papa', 'Cebolla', 'Zanahoria']
---
+++ calculadora.js
@@ -0,0 +1,10 @@
+calculadora = {
+    sumar : function(num1,num2) {
+        return num1 + num2
+    },
+    restar : function (num1,num2) {
+        return num1 - num2
+    }
+}
+
+module.exports (calculadora)
