c06f703a5e4bf95672f9026b90bd4723038e63d8
Cristian <cristiannicolas32@hotmail.com>
2020-11-09 18:35:21 -0300

    Cambie tus archivos Cintia

--- trabajoduo1.js
+++ trabajoduo1.js
@@ -1,3 +1,6 @@
-var seriesFavorita = "Orange is the New Black!";
+var serieFavorita = "Orange is the New Black!";
 let palabra1 = serie.slice(14,16)
 console.log(palabra1)
+
+let series = ['Ratchet','Las chicas del cable']
+let agregarMas = series.push(serieFavorita)
